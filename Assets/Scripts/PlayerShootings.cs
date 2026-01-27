e96594fce4225b2f1cc15bbcec7780f03fa491b8
Vũ Quang Dũng <vudung012358@gmail.com>
2026-01-27 17:00:01 +0700

    DEMO Part 4

--- Assets/Scripts/PlayerShootings.cs
+++ Assets/Scripts/PlayerShootings.cs
@@ -3,22 +3,29 @@ public class PlayerShootings : MonoBehaviour
 {
     public GameObject bulletPrefabs;
     public float shootingInterval;
+    public Vector3 bulletOffset;
+
     private float lastBulletTime;
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            if (Time.time - lastBulletTime >
-            shootingInterval)
-            {
-                ShootBullet();
-                lastBulletTime = Time.time;
-            }
+            UpdateFiring();
+        }
+    }
+
+    private void UpdateFiring()
+    {
+        if (Time.time - lastBulletTime > shootingInterval)
+        {
+            ShootBullet();
+            lastBulletTime = Time.time;
         }
     }
 
     private void ShootBullet()
     {
-        Instantiate(bulletPrefabs, transform.position, transform.rotation);
+        var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
     }
 }
